4e70801d509225760a5c80c54350dc46753d769b
Lara <debbiegterra@gmail.com>
2022-01-13 13:17:38 +0100

    created the logic behind displaying the users name on the question's page

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
+  name: string = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.name = localStorage.getItem('name')!;
   }
-
 }
