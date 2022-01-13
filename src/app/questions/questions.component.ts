3c4cbb759a5b6211aee2236c0ab73b6791e22299
Lara <debbiegterra@gmail.com>
2022-01-13 19:29:00 +0100

    created a service to get the questions from a json file and added logic to display each questions and options to the user

--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from '../services/questions.service';
 
 @Component({
   selector: 'app-questions',
@@ -8,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 export class QuestionsComponent implements OnInit {
 
   name: string = '';
-  constructor() { }
+  questionList: any = [];
+  currentQuestion: number = 0;
+  points: number = 0;
+  timer: number = 60;
+  constructor(private questionService: QuestionsService) { }
 
   ngOnInit(): void {
     this.name = localStorage.getItem('name')!;
+    this.getAllQuestions();
+  }
+
+  getAllQuestions() {
+    this.questionService.getQuestion()
+    .subscribe(
+      (res) => {
+      this.questionList = res.questions;
+    }) 
+  }
+
+  nextQuestion() {
+    this.currentQuestion++;
+  }
+
+  prevQuestion() {
+    this.currentQuestion--;
   }
 }
