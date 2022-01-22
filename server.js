46f1a10b5d339298e799d35162d3003c8d11a085
Lara <debbiegterra@gmail.com>
2022-01-22 19:05:17 +0100

    fixed a bug in the server file

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-quiz-app'));
-
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
-);
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const ngApp = express();
+ngApp.use(express.static('./dist/Angular-Quiz-App'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/Angular-Quiz-App/index.html'));
+});
+ngApp.listen(process.env.PORT || 8080);
