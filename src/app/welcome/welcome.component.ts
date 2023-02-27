dc199ceb7d5141d00a721956dd5d0aef76770317
Omolara5861 <debbiegterra@gmail.com>
2023-02-27 14:04:29 +0100

    docs: :bulb: Add comment to welcome component

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,8 @@
+// This imports the necessary modules from Angular
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup, FormControlName, Validators } from '@angular/forms';
+
+// This component is responsible for the welcome screen of the app
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -7,21 +10,25 @@ import { FormControl, FormGroup, FormControlName, Validators } from '@angular/fo
 })
 export class WelcomeComponent implements OnInit {
 
+  // This creates a form group with a single form control for the user's name
   form = new FormGroup({
     user:new FormControl('', [Validators.required, Validators.pattern('[a-z A-Z]+$')])
   })
 
+  // This gets a reference to the name input field in the template
   @ViewChild('name')nameKey!: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  /** This method is called when the user clicks the start quiz button, it saves the user's name in local storage */
   startQuiz() {
     localStorage.setItem('name', this.nameKey.nativeElement.value);
   }
 
+  /** This getter method returns the form control for the user's name */
   get user() {
     return this.form.get('user');
-    
   }
 }
