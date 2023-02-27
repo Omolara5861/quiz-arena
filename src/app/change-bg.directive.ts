98b07fa38c9b6f8b08932b8f5c5b5f28e67aa505
Omolara5861 <debbiegterra@gmail.com>
2023-02-27 14:08:01 +0100

    docs: :bulb: Add comment to custom directive

--- src/app/change-bg.directive.ts
+++ src/app/change-bg.directive.ts
@@ -2,25 +2,20 @@ import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/
 import { QuestionsComponent } from './questions/questions.component';
 
 @Directive({
-  selector: '[appChangeBg]'
+  selector: '[appChangeBg]' // Selector used to apply the directive
 })
 export class ChangeBgDirective {
 
-  @Input() isCorrect: Boolean = false;
+  @Input() isCorrect: Boolean = false; // Input property that determines whether the answer is correct or not
   constructor(private el: ElementRef, private render: Renderer2) { }
 
-  @HostListener('click') answer() {
-    if (this.isCorrect && !QuestionsComponent.hasAnswered ) {
-      // this.render.setStyle(this.el.nativeElement, 'background', 'darkGreen');
-      // this.render.setStyle(this.el.nativeElement, 'color', 'white');
-      this.render.addClass(this.el.nativeElement, 'correct');
+  @HostListener('click') answer() { // Host listener that listens for a click event
+    if (this.isCorrect && !QuestionsComponent.hasAnswered ) { // Condition to check if the answer is correct and has not been answered yet
+      this.render.addClass(this.el.nativeElement, 'correct'); // Adds 'correct' class to the element
     }
-    else if(!QuestionsComponent.hasAnswered) {
-      // this.render.setStyle(this.el.nativeElement, 'background', 'red');
-      // this.render.setStyle(this.el.nativeElement, 'color', 'white');
-      this.render.addClass(this.el.nativeElement, 'incorrect')
+    else if(!QuestionsComponent.hasAnswered) { // Condition to check if the answer has not been answered yet
+      this.render.addClass(this.el.nativeElement, 'incorrect') // Adds 'incorrect' class to the element
     }
-  } 
+  }
 
 }
-
